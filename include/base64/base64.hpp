b5a6e18185b15f641578a911f9f8409d1ec6653b
Falk Werner <falk.werner@gmx.net>
2026-05-19 19:11:11 +0200

    refactor and fix base64 implentation
    - switch to C++ implementation to match usage
    -  fix underflow error during base64 decode when data is less than 4 bytes
    - add detection of invalid characters
    - add detection of invalid length of base64 encoded data
    - fix invalid interpretation of '=' characters in base64 data
    - add test cases for some edge cases
    - implement strict base64 decoding
    
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

---
+++ include/base64/base64.hpp
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef BASE64_HPP
+#define BASE64_HPP
+
+#include <string>
+#include <vector>
+#include <cinttypes>
+
+namespace base64
+{
+
+std::string encode(uint8_t const * buffer, size_t length);
+
+void decode(std::string const &from, std::vector<uint8_t> &to);
+
+
+}
+
+#endif
